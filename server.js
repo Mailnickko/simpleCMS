c75a5e5971d6ad120b61bc6335d34b09bb0f8e52
Nick Ko <mailnickko@gmail.com>
2016-03-10 00:46:16 -0800

    updates server.json

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 //Body Parser
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
@@ -17,6 +15,6 @@ require('./server/config/mongoose.js');
 require('./server/config/server_routes.js')(app);
 
 //Server
-app.listen(app.get('port'), function(){
- console.log("listening on port ", app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
